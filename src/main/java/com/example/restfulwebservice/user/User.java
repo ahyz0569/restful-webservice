6332ab784f83ec25821a1f78008414a11e097afb
Yunok <ahyz0569@gmail.com>
2021-02-18 19:13:29 +0900

    Update API 구현

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,12 +3,18 @@ package com.example.restfulwebservice.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
+
+    @Min(value = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
+
+    @Past
     private LocalDate joinDate;
 }
